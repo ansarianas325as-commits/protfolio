09be7d65be1f32331ce02ecd753f0b4c99875f22
ansarianas325as-commits <ansarianas325.as@gmail.com>
2026-08-21 19:26:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const toggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".primary-nav");
 
-toggle.addEventListener("click", () => {
-  nav.classList.toggle("open");
-});
